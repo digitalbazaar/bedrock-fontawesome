ce2ffeb28d680fa86a5acb72320bc0cdeab69031
David I. Lehn <dlehn@digitalbazaar.com>
2019-05-13 22:26:26 -0400

    Add initial files.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
