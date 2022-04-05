e6fce7c510711bd4cbad6c0d79905e9327d90358
Dave Longley <dlongley@digitalbazaar.com>
2022-04-05 11:10:17 -0400

    Convert to module (ESM).

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /*!
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
-
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/regular.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
